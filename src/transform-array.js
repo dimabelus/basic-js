e73248075d4d1a848e2e4a204d555fef1bfb0d11
dimabelus <dima21belus@gmail.com>
2021-03-28 18:51:02 +0300

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let finalArray = [];
+
+  if (!Array.isArray(arr)) throw new Error();
+  if (arr.length === 0) return arr;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === undefined) {
+      continue;
+    }
+    switch (arr[i]) {
+      case `--discard-next`:
+        finalArray.push("discardThatElement");
+        i++;
+        break;
+      case `--discard-prev`:
+        if (finalArray.length > 0) {
+          finalArray.pop();
+        }
+        break;
+      case `--double-next`:
+        if (arr.length - 1 > i) {
+          finalArray.push(arr[i + 1]);
+        }
+        break;
+      case `--double-prev`:
+        if (finalArray.length > 0) {
+          finalArray.push(finalArray[finalArray.length - 1]);
+        }
+        break;
+      default:
+        finalArray.push(arr[i]);
+        break;
+    }
+  }
+
+  finalArray = finalArray.filter((element) => element !== "discardThatElement");
+  return finalArray;
 };
