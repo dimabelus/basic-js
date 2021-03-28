5a27e5ab5f035672a8911fc0c09334338f8933ec
dimabelus <dima21belus@gmail.com>
2021-03-28 20:33:46 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let finalString = "";
+  options.separator === undefined ? (options.separator = "+") : "";
+  options.additionSeparator === undefined
+    ? (options.additionSeparator = "|")
+    : "";
+  options.repeatTimes === undefined ? (options.repeatTimes = 1) : "";
+  options.additionRepeatTimes === undefined
+    ? (options.additionRepeatTimes = 1)
+    : "";
+  options.addition === undefined ? (options.additionRepeatTimes = "") : "";
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    finalString += str;
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      finalString += options.addition;
+      j < options.additionRepeatTimes - 1
+        ? (finalString += options.additionSeparator)
+        : "";
+    }
+    i < options.repeatTimes - 1 ? (finalString += options.separator) : "";
+  }
+  return finalString;
 };
-  
